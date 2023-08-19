cf1ff3fde0d1222f0ac017289e9aa72dff4cd971
RJ Barro <codelinguist@gmail.com>
2023-08-19 14:43:16 -0400

    Placeholder test projects

---
+++ CarePatron.Application.Integration.Tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace CarePatron.Application.Integration.Tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
